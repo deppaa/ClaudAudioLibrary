537703fcb923e872c53bfde1d67b6e3fba4f1477
dappaa <radmir1118@gmail.com>
2020-10-01 15:39:18 +0500

    creating booksList actions

---
+++ components/Store/BookList/actions.js
@@ -0,0 +1,45 @@
+import { LOAD_BOOKS_LIST, PRELOADER_STATE } from './types'
+import YandexDisk from '../../Hoock/YandexDisk'
+
+const { getResources } = YandexDisk()
+
+export const putData = (data) => {
+	return {
+		type: LOAD_BOOKS_LIST,
+		payload: data
+	}
+}
+
+export const showPreloader = (prop) => {
+	return {
+		type: PRELOADER_STATE,
+		payload: prop
+	}
+}
+
+export const loadBooksList = () => async (dispatch, getState) => {
+	dispatch(showPreloader(true))
+	try {
+		const result = await getResources({
+			url: '/CloudAudioBookPayer',
+			fields: ['_embedded.items.name', '_embedded.items.path', '_embedded.items.custom_properties', '_embedded.items.resource_id']
+		})
+		const data = result.data._embedded.items
+
+		for (let i = 0; i < data.length; i++) {
+			const element = data[i].path.replace('disk:', '')
+
+			const img = await getResources({
+				url: element,
+				media_type: 'image'
+			})
+
+			data[i].image = img.data[0].file
+		}
+
+		dispatch(putData(data))
+		dispatch(showPreloader(false))
+	} catch (error) {
+		console.log(error)
+	}
+}
