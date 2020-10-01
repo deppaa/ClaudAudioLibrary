418a8ffc280053575e22353f9fa1cdb2ab4cf75e
dappaa <radmir1118@gmail.com>
2020-10-01 17:31:39 +0500

    add screen height and flatlist emptycomponent

--- components/BookList/BookList.js
+++ components/BookList/BookList.js
@@ -3,15 +3,15 @@ import {
 	FlatList,
 	SafeAreaView,
 	TouchableOpacity,
-	StyleSheet
+	StyleSheet,
+	Dimensions
 } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import ElementItem from '../ElementItem/ElementItem'
 import { loadBooksList } from '../Store/BookList/actions'
-import {
-	BarIndicator
-} from 'react-native-indicators';
+import { BarIndicator } from 'react-native-indicators';
 
+const ScreenHeight = Dimensions.get("window").height;
 
 const BookList = () => {
 	const dispatch = useDispatch()
@@ -20,21 +20,26 @@ const BookList = () => {
 		dispatch(loadBooksList())
 	}, [dispatch])
 
+	const preloader = () => {
+		return (
+			<BarIndicator style={styles.preloader} color='#FFCE33' size={40} />
+		)
+	}
+
 	const bookListState = useSelector(({ BooksList }) => {
 		return {
 			data: BooksList.data,
-			preloader: BooksList.preloader
 		}
 	})
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<BarIndicator color='#FFCE33' size={40} style={{ display: bookListState.preloader ? 'flex' : 'none' }} />
 			<FlatList
-				style={{ display: !bookListState.preloader ? 'flex' : 'none' }}
+				style={styles.listData}
 				data={bookListState.data}
 				renderItem={({ item }) => <TouchableOpacity onPress={() => alert(item.name)}><ElementItem poster={item.image} text={item.name} progress={0} /></TouchableOpacity>}
 				keyExtractor={item => item.resource_id}
+				ListEmptyComponent={preloader}
 			/>
 		</SafeAreaView>
 	)
@@ -44,6 +49,12 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1
 	},
+	listData: {
+		flex: 1,
+	},
+	preloader: {
+		height: ScreenHeight
+	}
 })
 
 export default BookList
