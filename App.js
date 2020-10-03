f4a5fc228b1f2038d3b7547b9a38775dbfd48427
deppaa <radmir1118@gmail.com>
2020-10-04 02:06:35 +0500

    add AppNavigation

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { Provider } from 'react-redux'
 import PlayerScreens from './screens/PlayerScreens/PlayerScreens'
 import BookListScreens from './screens/BookListScreens/BookListScreens'
 import store from './components/Store/reducers'
+import { AppNavigation } from './navigation/AppNavigation'
 export default App = () => {
 	return (
 		<Provider store={store}>
-			<BookListScreens />
+			<AppNavigation />
 		</Provider>
 	);
 };
