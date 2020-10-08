3ae416b270b9bf414b6ed6761370c1e170dee70c
dappaa <radmir1118@gmail.com>
2020-10-08 12:32:52 +0500

    add stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import store from './components/Store/reducers'
 import PlayerScreens from './screens/PlayerScreens/PlayerScreens'
 import BookListScreens from './screens/BookListScreens/BookListScreens'
-import store from './components/Store/reducers'
-import { AppNavigation } from './navigation/AppNavigation'
+import SettingsScreens from './screens/SettingsScreens/SettingsScreens'
+
+const Stack = createStackNavigator()
+
+const AppNavigation = () => {
+	return (
+		<Stack.Navigator headerMode={"none"}>
+			<Stack.Screen name="BookList" component={BookListScreens} />
+			<Stack.Screen name="Player" component={PlayerScreens} />
+			<Stack.Screen name="Settings" component={SettingsScreens} />
+		</Stack.Navigator>
+	);
+}
+
 export default App = () => {
 	return (
 		<Provider store={store}>
-			<AppNavigation />
+			<NavigationContainer>
+				<AppNavigation />
+			</NavigationContainer>
 		</Provider>
 	);
 };
